8b168eb9c9c008599eb053804b19a7d7b21c656d
Michael Roberts <michaelroberts@Michaels-MacBook-Air.local>
2018-07-03 14:46:48 -0700

    adds local datasource for mongodb

---
+++ server/local.datasources.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "mongo": {
+    "host": "127.0.0.1",
+    "port": 27017,
+    "url": "",
+    "database": "testdb",
+    "password": "",
+    "name": "mongo",
+    "user": "",
+    "connector": "mongodb"
+  }
+}
