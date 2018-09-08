db5216ab561dac507ec006d6a4dbc72078377d1b
psubing <psubing@gmail.com>
2018-09-07 18:17:00 -0700

    aptrequests file

---
+++ src/components/Home/AptRequests.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import moment from 'moment'
+
+class AptRequests extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      requests: []
+    }
+  }
+
+  componentDidMount() {
+    if (!this.props.userId) return;
+    const currentDate = new Date();
+    const currentDateIsoFormat = currentDate.toISOString();
+    axios.get(`/api/AptRequests?filter[where][time][gt]=` + currentDateIsoFormat)
+      .then((response) => {
+        console.log(response.data)
+        this.setState({
+          requests: response.data
+        })
+        console.log(this.state.requests)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    console.log(this.state.requests)
+    return (
+      <div>
+        <h2>Appointment Requests</h2>
+        {this.state.requests ? <table className='table'>
+          <thead>
+            <tr>
+              {/* <th>Name</th> */}
+              <th>Summary</th>
+              <th>Description</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Approve</th>
+              <th>Deny</th>
+            </tr>
+          </thead>
+          <tbody className='table-striped'>
+            {this.state.requests.map((e) => {
+              return (
+                <tr>
+                  <td>{e.topicSummary}</td>
+                  <td>{e.issueDescription}</td>
+                  <td>{moment(e.time).format('L')}</td>
+                  <td>{moment(e.time).format('hh:MM a')}</td>
+                  <td><button type='button' className='btn btn-success'>Approve</button></td>
+                  <td><button type='button' className='btn btn-danger'>Deny</button></td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+          : <div className="alert alert-info">You haven't scheduled any appointments yet!</div>
+        }
+      </div>
+    )
+  }
+}
+
+export default AptRequests;
