ba4ff0e400a80f8e6d9a6a4443b3d3e4a78ce314
TheStaches <Eric.B.Dodds@gmail.com>
2018-09-27 14:56:07 -0700

    Merging conflicts

--- server/boot/create-admin.js
+++ server/boot/create-admin.js
@@ -115,5 +115,42 @@ module.exports = (app) => {
             });
         });
     });
+  Visitor.findOrCreate({
+    where: {
+      email: 'michael@origincodeacademy.com',
+    },
+  },
+    {
+      email: 'michael@origincodeacademy.com',
+      firstName: 'Michael',
+      lastName: 'Roberts',
+      password: process.env.ADMIN_PASSWORD3,
+      emailVerified: true,
+    },
+    (err, visitor) => {
+      Role.findOrCreate({
+        where: {
+          name: 'ADMIN',
+        },
+      },
+        {
+          name: 'ADMIN',
+        },
+        (err, role) => {
+          if (err) console.log('error creating role', err);
+          RoleMapping.findOrCreate({
+            where: {
+              principalType: 'ADMIN',
+              principalId: visitor.id,
+            },
+          },
+            {
+              principalType: 'ADMIN',
+              principalId: visitor.id,
+            }, (error, mapping) => {
+              if (err) console.log(err);
+            });
+        });
+    });
 };
 
