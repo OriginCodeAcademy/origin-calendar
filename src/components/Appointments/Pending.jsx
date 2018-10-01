807320d2cc3b4c25f62732fb5599724c9c857a01
Kaiya Walker <kaiyawalker@mail.com>
2018-10-01 16:21:01 -0700

    trash icon

--- src/components/Appointments/Pending.jsx
+++ src/components/Appointments/Pending.jsx
@@ -79,16 +79,21 @@ class PendingAppointments extends Component {
         <h2>Pending Appointments</h2>
 
         {this.state.pending.length ? <table className='table'>
+       
           <thead>
             <tr>
               <th>Summary</th>
               <th>Description</th>
               <th>Date</th>
               <th>Time</th>
+              <th></th>
+
               
             </tr>
             
+            
           </thead>
+          
           <tbody className='table-striped'>
 
             {this.state.pending.map((e) => {
@@ -99,18 +104,25 @@ class PendingAppointments extends Component {
                   <td>{moment(e.time).format('L')}</td>
                   <td>{moment(e.time).format('hh:mm a')}</td>
                   <td>
-                  <button className="btn btn-info" instructorId={e.instructorId} email={e.email} studentName={e.studentName} id={e.id} time={e.time} onClick={this.handleRemoveApt}>Remove</button>
+                  {/* <button class="btn btn-danger" instructorId={e.instructorId} email={e.email} studentName={e.studentName} id={e.id} time={e.time} onClick={this.handleRemoveApt}>X</button> */}
+                  <svg  className='trash'instructorId={e.instructorId} email={e.email} studentName={e.studentName} id={e.id} time={e.time} onClick={this.handleRemoveApt} version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><title>bin</title><path d="M4 10v20c0 1.1 0.9 2 2 2h18c1.1 0 2-0.9 2-2v-20h-22zM10 28h-2v-14h2v14zM14 28h-2v-14h2v14zM18 28h-2v-14h2v14zM22 28h-2v-14h2v14z"></path><path d="M26.5 4h-6.5v-2.5c0-0.825-0.675-1.5-1.5-1.5h-7c-0.825 0-1.5 0.675-1.5 1.5v2.5h-6.5c-0.825 0-1.5 0.675-1.5 1.5v2.5h26v-2.5c0-0.825-0.675-1.5-1.5-1.5zM18 4h-6v-1.975h6v1.975z"></path></svg>
                   </td>
                 </tr>
+                
+                
               )
             })}
           </tbody>
         </table>
+        
           : <div className="alert alert-info">You haven't scheduled any appointments yet!</div>
         }
       </div>
+      
+   
     )
   }
 }
+
 
 export default PendingAppointments;
